a536fcaf4ece95a71a2acd98253d70895d8adbca
Samira <samirasilvamjj@gmail.com>
2023-03-09 10:44:23 -0300

    exercicio HOFs dia 01

---
+++ Fundamentos/3.-Introdução-à-JS/Dia-03-Funções/script.js
@@ -0,0 +1,22 @@
+let saldo = 100;
+
+function somaSaldo(valor) {
+  return valor + saldo;
+};
+
+function subtraiSaldo(valor) {
+  return valor - saldo;
+};
+
+function multiplicaSaldo(valor) {
+  return valor * saldo;
+};
+
+function divideSaldo(valor) {
+  valor / saldo;
+};
+
+console.log(somaSaldo(10));
+console.log(subtraiSaldo(20));
+console.log(multiplicaSaldo(5));
+console.log(divideSaldo(2));
