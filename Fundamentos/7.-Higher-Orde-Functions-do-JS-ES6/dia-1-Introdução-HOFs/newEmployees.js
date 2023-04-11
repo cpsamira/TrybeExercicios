97245ea1120979462648e94aa98df384c412f449
Samira <samirasilvamjj@gmail.com>
2023-04-11 15:27:02 -0300

    exercicio

--- Fundamentos/7.-Higher-Orde-Functions-do-JS-ES6/dia-1-Introdução-HOFs/newEmployees.js
+++ Fundamentos/7.-Higher-Orde-Functions-do-JS-ES6/dia-1-Introdução-HOFs/newEmployees.js
@@ -1,16 +1,13 @@
-const newEmployees = (employeeGenerator) => {
+const newEmployees = (callback) => {
   const employees = {
-    id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: employeeGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: employeeGenerator('Carla Paiva') // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: callback('Pedro Guerra')// Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
 
 const employeeGenerator = (fullName) => {
-  const email = fullName.toLowerCase().split(' ').join('_');
-  return {fullName, email: `${email}@betrybe.com`};
-  
+  const email = fullName.toLowerCase().replace(' ', '_');
+  return { fullName, email: `${email}@trybe.com`};
 }
-
-console.log(newEmployees(employeeGenerator));
